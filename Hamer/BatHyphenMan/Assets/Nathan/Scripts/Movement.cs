68e9251e8cab0836dc9691b3e05bf1d850068507
NathLFox <47029615+NathLFox@users.noreply.github.com>
2020-02-03 19:32:21 +0000

    Root Motion!
    Think i've about cracked Root Motion guys - animation looks super clean!
    Check out my folder in the project and test the 'MainTest' scene out!

---
+++ Hamer/BatHyphenMan/Assets/Nathan/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator anim;
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("walkForward", true);
+        }
+        else
+        {
+            anim.SetBool("walkForward", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("runForward", true);
+        }
+        else
+        {
+            anim.SetBool("runForward", false);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("turnLeft", true);
+        }
+        else
+        {
+            anim.SetBool("turnLeft", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("turnRight", true);
+        }
+        else
+        {
+            anim.SetBool("turnRight", false);
+        }
+    }
+}
