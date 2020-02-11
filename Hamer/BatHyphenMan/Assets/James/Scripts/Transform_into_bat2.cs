e3069d64ce6629bbe10462376c6120b47f64519b
JamesWyatt01 <44027260+JamesWyatt01@users.noreply.github.com>
2020-02-11 18:13:50 +0000

    Made 2 scripts for the bat
    Script 1 is bat movement. Move forward and back with W and S, rotate with A and D, move up and down with Arrow up and down keys. Scipt 2 is transform to bat but just without the cooldown.

---
+++ Hamer/BatHyphenMan/Assets/James/Scripts/Transform_into_bat2.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform_into_bat2 : MonoBehaviour
+{
+    public GameObject Vampire;
+    public GameObject Bat;
+
+    public bool Vampire_Is_Active;
+    public bool Bat_Is_Active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bat.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vampire.activeSelf)
+        {
+            Vampire_Is_Active = true;
+        }
+
+        if (!Vampire.activeSelf)
+        {
+            Vampire_Is_Active = false;
+        }
+
+        if (Bat.activeSelf)
+        {
+            Bat_Is_Active = true;
+        }
+
+        if (!Bat.activeSelf)
+        {
+            Bat_Is_Active = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Vampire_Is_Active == true)
+            {
+                Bat.SetActive(true);
+
+                Bat.transform.position = Vampire.transform.position;
+                Bat.transform.rotation = Vampire.transform.rotation;
+
+                Vampire.SetActive(false);
+            }
+
+            if (Bat_Is_Active == true)
+            {
+                Vampire.SetActive(true);
+
+                Vampire.transform.position = Bat.transform.position;
+                Vampire.transform.rotation = Bat.transform.rotation;
+
+                Bat.SetActive(false);
+            }
+        }
+    }
+}
