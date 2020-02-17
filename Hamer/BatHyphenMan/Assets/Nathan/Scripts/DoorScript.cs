e25e37556a1981e786518afff9b983c1acc6ac85
NathLFox <NathLFox@outlook.com>
2020-02-17 14:14:36 +0000

    Door Anim
    Doors will now open and close with the press of E

---
+++ Hamer/BatHyphenMan/Assets/Nathan/Scripts/DoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+      if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetBool("DoorOpen", true);
+                anim.SetBool("DoorClosed", true);
+            }
+            else
+            {
+                anim.SetBool("DoorOpen", false);
+                anim.SetBool("DoorClosed", false);
+            }
+        }
+    }
+}
