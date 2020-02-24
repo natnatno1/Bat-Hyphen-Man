d87408db2355f373f5c561a08b62f17d7f920ca6
natnatno1 <44027070+natnatno1@users.noreply.github.com>
2020-02-24 12:38:35 +0000

    Natalie
    -Camera Controls Improved

---
+++ Hamer/BatHyphenMan/Assets/Natalie/Scripts/Camera_Collision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Collision : MonoBehaviour
+{
+    public float MinDistance = 1.0f;
+    public float MaxDistance = 4.0f;
+    public float Smooth = 10.0f;
+    Vector3 DollyDirection;
+    public Vector3 DollyDirectionAdjusted;
+    public float Distance;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        DollyDirection = transform.localPosition.normalized;
+        Distance = transform.localPosition.magnitude;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 DesiredCameraPosition = transform.parent.TransformPoint(DollyDirection * MaxDistance);
+        RaycastHit Hit;
+
+        if (Physics.Linecast(transform.parent.position, DesiredCameraPosition, out Hit))
+        {
+            Distance = Mathf.Clamp((Hit.distance * 0.87f), MinDistance, MaxDistance);
+
+        }
+
+        else
+        {
+            Distance = MaxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, DollyDirection * Distance, Time.deltaTime * Smooth);
+
+    }
+}
