32a1726a141693c8b59df6b8f1e6c2ac6bba042b
natnatno1 <44027070+natnatno1@users.noreply.github.com>
2020-02-24 15:19:04 +0000

    Natalie
    -Better Camera Control

--- Hamer/BatHyphenMan/Assets/Natalie/Scripts/Camera_Rotation.cs
+++ Hamer/BatHyphenMan/Assets/Natalie/Scripts/Camera_Rotation.cs
@@ -4,48 +4,21 @@ using UnityEngine;
 
 public class Camera_Rotation : MonoBehaviour
 {
-    public float CameraMoveSpeed = 120f;
-    public GameObject CameraFollowObject;
-    Vector3 FollowPosition;
-    public float ClampAngle = 80.0f;
-    public float InputSensitivity = 150.0f;
-    public GameObject CameraObject;
-    public GameObject PlayerObject;
-    public float CamDistanceXToPlayer;
-    public float CamDistanceYToPlayer;
-    public float CamDistanceZToPlayer;
-    public float MouseX;
-    public float MouseY;
-    public float FinalInputX;
-    public float FinalInputZ;
-    public float SmoothX;
-    public float SmoothY;
-    private float RotY = 0.0f;
-    private float RotX = 0.0f;
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Vector3 Rot = transform.localRotation.eulerAngles;
-        RotY = Rot.y;
-        RotX = Rot.x;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-    }
+    //Here I declare the speed of rotation
+    public float RotSpeed = 6f;
+    //rotation on y
+    float rotY;
+    //Clamping Angle
+    public float ClampAngle = 10f;
+    
 
     // Update is called once per frame
     void Update()
     {
-        //We setup the rotation of the sticks here
-
-        float InputX = Input.GetAxis("RightStickHorizontal");
-        float InputZ = Input.GetAxis("RightStickVertical");
-        MouseX = Input.GetAxis("Mouse X");
-        MouseY = Input.GetAxis("Mouse Y");
-
+        rotY += Input.GetAxis("Mouse Y") * RotSpeed;
+        rotY = Mathf.Clamp(rotY, -ClampAngle, ClampAngle);
+        transform.localRotation = Quaternion.Euler(-rotY, 0f, 0f);
 
     }
+
 }
