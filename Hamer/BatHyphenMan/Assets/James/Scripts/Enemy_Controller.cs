dc9d230d53113f64c436039b0527f47ff6b24679
JamesWyatt01 <44027260+JamesWyatt01@users.noreply.github.com>
2020-03-28 14:59:16 +0000

    Combat improvments
    In the new combat scene in my folder is an enemy that moves better using a navmesh.

--- Hamer/BatHyphenMan/Assets/James/Scripts/Enemy_Controller.cs
+++ Hamer/BatHyphenMan/Assets/James/Scripts/Enemy_Controller.cs
@@ -1,80 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_Controller : MonoBehaviour
 {
     public Transform Player;
-    public float Enemy_chase_distance = 1.5f;
-    public float Enemy_chase_distance2 = 4f;
+    public float EnemyChaseMin = 1.6f;
+    public float EnemyChaseMax = 30f;
     public Game_Manager GM;
     public Animator Enemy_anim;
     public Animator Player_anim;
-
     private float nextActionTime = 0.0f;
     public float period;
-    public bool Move_Restriction;
+    public bool InstantAttack;
+    private NavMeshAgent Nav;
+    public bool EnemyActivated;
 
-    // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("HumanForm").GetComponent<Transform>();
         GM = GameObject.Find("GameManager").GetComponent<Game_Manager>();
         Enemy_anim = GetComponent<Animator>();
         Player_anim = GameObject.Find("Vampire").GetComponentInChildren<Animator>();
-        period = Random.Range(1f, 5f);
-        Move_Restriction = true;
+        period = Random.Range(3f, 5f);
+        InstantAttack = true;
+        Nav = GetComponent<NavMeshAgent>();
+        EnemyActivated = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Player); //Look at the player
+        transform.LookAt(Player);
 
-        if (Vector3.Distance(transform.position, Player.transform.position) > Enemy_chase_distance2)
+        if (Vector3.Distance(transform.position, Player.transform.position) <= EnemyChaseMax)
         {
-            if (Vector3.Distance(transform.position, Player.transform.position) > Enemy_chase_distance)
+            RaycastHit hitinfo;
+            if (Physics.Raycast(transform.position, transform.forward, out hitinfo))
+            {
+                if (hitinfo.collider.gameObject.tag == "Player")
+                {
+                    EnemyActivated = true;
+                }
+            }
+        }
+
+        if (EnemyActivated == true)
+        {
+            if (Vector3.Distance(transform.position, Player.transform.position) > 1.5f)
             {
                 Enemy_anim.SetBool("IsAttacking", false);
                 Enemy_anim.SetBool("IsWalking", true);
-                transform.position += transform.forward * Time.deltaTime * 1.5f;
+                Nav.enabled = true;
+                Nav.destination = Player.transform.position;
             }
-        }
-        if (Vector3.Distance(transform.position, Player.transform.position) <= Enemy_chase_distance2)
-        {
-            if (Vector3.Distance(transform.position, Player.transform.position) > Enemy_chase_distance)
+            if (Vector3.Distance(transform.position, Player.transform.position) < 1.5f)
             {
-                if (Move_Restriction == true)
+                Enemy_anim.SetBool("IsWalking", false);
+                Nav.enabled = false;
+
+                if (InstantAttack == true)
                 {
-                    Enemy_anim.SetBool("IsAttacking", false);
-                    Enemy_anim.SetBool("IsWalking", false);
-                    transform.position += transform.forward * Time.deltaTime * 0f;
+                    Enemy_anim.SetBool("IsAttacking", true);
+                    Invoke("StopAttack", 0.5f);
+                }
+                if (InstantAttack == false)
+                {
+                    if (Time.time > nextActionTime)
+                    {
+                        nextActionTime += period;
+                        Enemy_anim.SetBool("IsAttacking", true);
+                        Invoke("StopAttack", 0.5f);
 
-                    Invoke("Enemy_Attack", 5f);
-                }             
+                    }
+                }
             }
-        }
-    }
-
-    void Enemy_Attack()
-    {
-        Move_Restriction = false;
-        if (Vector3.Distance(transform.position, Player.transform.position) > Enemy_chase_distance)
-        {
-            Enemy_anim.SetBool("IsWalking", true);
-            transform.position += transform.forward * Time.deltaTime * 1.5f;
-        }
-        if (Vector3.Distance(transform.position, Player.transform.position) <= Enemy_chase_distance)
-        {
-            Enemy_anim.SetBool("IsAttacking", true);
-            Invoke("StopAttack", 0.2f);
-            Enemy_anim.SetBool("IsWalking", false);
-            transform.position += transform.forward * Time.deltaTime * 0f;
         }
     }
 
     void StopAttack()
     {
         Enemy_anim.SetBool("IsAttacking", false);
+        InstantAttack = false;
     }
 }
